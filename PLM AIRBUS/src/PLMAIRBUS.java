589042b99562cb2499ecf84697c881825d1b8fdc
Mohamed BOUCHERBA <mboucherba31@gmail.com>
2024-09-19 15:53:11 +0200

    add parts to airplane

--- PLM AIRBUS/src/PLMAIRBUS.java
+++ PLM AIRBUS/src/PLMAIRBUS.java
@@ -35,23 +35,72 @@ public class PLMAIRBUS {
 		 airplanes.add(new String[] {"6", "A380","clôturé", "avions d’affaires"});
 		 airplanes.add(new String[] {"7", "A380","clôturé", "Fret"});
 		 
+		 // pièce
+		 ArrayList<String[]> parts = new ArrayList<>();
+		 
 		 Scanner scan = new Scanner(System.in);
 		 //1/ Votre application doit permettre d’afficher tous les avions (id, programme, phase, type) si 
 		 //l’utilisateur le souhaite.
-		 showAirplans(scan, airplanes);
+		 showAirplans(scan, airplanes,parts);
 		 
+		 //Vous devez gérer les saisies erronées ?
 		 addAirplaine(scan, airplanes);
+		 showAirplans(scan, airplanes,parts);
 		
-		
+		 addPartToAirplane(scan, airplanes, parts);
+		 showAirplans(scan, airplanes,parts);
 		//2/ Rechercher une liste d’avions à partir d’un mot clé affichant tous les avions dont le programme 
 		//contient par ex la lettre A ou 320 ou A350…
 		searchAirplane(scan, airplanes);
 		
 	}
-	public static void showAirplans(Scanner scan, ArrayList<String[]> airplanes) {
+	public static void addPartToAirplane(Scanner scan, ArrayList<String[]> airplanes, ArrayList<String[]> parts) {
+		
+		System.out.println("Souhaitez-vous ajouter une pièce (Oui/Non)? ");
+		String response = scan.nextLine();
+		while(!response.equalsIgnoreCase("oui") && !response.equalsIgnoreCase("non")) {
+			System.out.println("Souhaitez-vous ajouter une avion (Oui/Non)?");
+			response = scan.nextLine();
+		}
+		while (response.equalsIgnoreCase("oui")) {
+			System.out.println("Entrez l'ID de l'avion pour ajouter une pièce:");
+	        String airplaineId = scan.nextLine();
+	        
+	        if (findAirplaneById(airplanes, airplaineId) != null) {
+	        	
+	            System.out.println("Entrez le nom de la pièce:");
+	            String name = scan.nextLine();
+	            System.out.println("Entrez la catégorie de la pièce:");
+	            String category = scan.nextLine();
+	            System.out.println("Entrez le prix de la pièce:");
+	            String price = scan.nextLine();
+	            parts.add(new String[] {airplaineId, name, category, price});
+	            System.out.println("Pièce ajoutée avec succès.");
+	            
+	        } else {
+	            System.out.println("Avion introuvable.");
+	        }
+	        System.out.println("Souhaitez-vous ajouter une pièce ? ");
+			response = scan.nextLine();
+			while(!response.equalsIgnoreCase("oui") && !response.equalsIgnoreCase("non")) {
+				System.out.println("Souhaitez-vous ajouter une pièce (Oui/Non)? ");
+				response = scan.nextLine();
+			}
+		}
+		
+	}
+	private static Object findAirplaneById(ArrayList<String[]> airplanes, String id) {
+        for (String[] airplane : airplanes) {
+            if (airplane[0].equals(id)) {
+                return airplane;
+            }
+        }
+        return null;
+	}
+	public static void showAirplans(Scanner scan, ArrayList<String[]> airplanes, ArrayList<String[]> parts) {
 		 String show = "";
 		 while (!show.equalsIgnoreCase("oui") && !show.equalsIgnoreCase("non")) {
-			 System.out.println("Souhaitez vous affiher la liste des avions ( Oui/Non ) ?");
+			 System.out.println("Souhaitez-vous affiher la liste des avions (Oui/Non) ?");
 			 show = scan.nextLine();
 		 };
 			 
@@ -65,42 +114,63 @@ public class PLMAIRBUS {
 					", phase: " + airplane[2] + 
 					", type: " + airplane[3]);
 					// System.out.println(String.join(" - ", airplan));
+					for(String[] part : parts) {
+						if( part[0].equals(airplane[0]) ) {
+							System.out.println("Nom de pièce: " + part[1] + ", Catégorie: " + part[2] + ",Prix: " + part[3]);
+						}
+					}
 				}
 			 
 		 }
 
 	}
 	public static void addAirplaine(Scanner scan, ArrayList<String[]>airplanes) {
-		// generate id
-				String id = String.valueOf(airplanes.size() + 1);
-				
-				String program = "";
-				String phase = "";
-				String type = "";
-				
-				
-				// program :
-				do {
-					System.out.println("Entrez un programme (ex: " + String.join(", ", validPrograms) + "):");
-					program = scan.nextLine().toUpperCase();
-				}
-				while ( !isValidProgram(program) || program.isEmpty());
-				
-				//phase :
-				do {
-					System.out.println("Entrez une phase (ex: " + String.join(", ", validPhases) + "):");
-					phase = scan.nextLine();
-				}
-				while ( !isValidPhase(phase) || phase.isEmpty());
-				
-				//type :
-				do {
-					System.out.println("Entrez une phase (ex: " + String.join(", ", validTypes) + "):");
-					type = scan.nextLine();
-				}
-				while ( !isValidType(type) || type.isEmpty());
-				
-				airplanes.add(new String[] {id, program, phase,type});
+		
+		System.out.println("Souhaitez-vous ajouter une avion (Oui/Non)?");
+		String response = scan.nextLine();
+		while(!response.equalsIgnoreCase("oui") && !response.equalsIgnoreCase("non")) {
+			System.out.println("Souhaitez-vous ajouter une avion (Oui/Non)?");
+			response = scan.nextLine();
+		}
+		while (response.equalsIgnoreCase("oui")) {
+			// generate id
+			String id = String.valueOf(airplanes.size() + 1);
+			
+			String program = "";
+			String phase = "";
+			String type = "";
+			
+			
+			// program :
+			do {
+				System.out.println("Entrez un programme (ex: " + String.join(", ", validPrograms) + "):");
+				program = scan.nextLine().toUpperCase();
+			}
+			while ( !isValidProgram(program) || program.isEmpty());
+			
+			//phase :
+			do {
+				System.out.println("Entrez une phase (ex: " + String.join(", ", validPhases) + "):");
+				phase = scan.nextLine();
+			}
+			while ( !isValidPhase(phase) || phase.isEmpty());
+			
+			//type :
+			do {
+				System.out.println("Entrez une phase (ex: " + String.join(", ", validTypes) + "):");
+				type = scan.nextLine();
+			}
+			while ( !isValidType(type) || type.isEmpty());
+			
+			airplanes.add(new String[] {id, program, phase,type});
+			System.out.println("Souhaitez-vous ajouter une avion ?");
+			response = scan.nextLine();
+			while(!response.equalsIgnoreCase("oui") && !response.equalsIgnoreCase("non")) {
+				System.out.println("Souhaitez-vous ajouter une avion (Oui/Non)?");
+				response = scan.nextLine();
+			}
+		}
+		
 	}
 	public static boolean isValidProgram(String program) {
 		for(String validProgram : validPrograms) {
